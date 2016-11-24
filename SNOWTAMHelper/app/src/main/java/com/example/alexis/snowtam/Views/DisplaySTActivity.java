7b8aad744c7f01afed72a7bd93b3801ab7e0b94b
DatagramBaguette <william.diakite@gmail.com>
2016-11-24 19:20:44 +0100

    petites corrections des xmls et Views

--- SNOWTAMHelper/app/src/main/java/com/example/alexis/snowtam/Views/DisplaySTActivity.java
+++ SNOWTAMHelper/app/src/main/java/com/example/alexis/snowtam/Views/DisplaySTActivity.java
@@ -1,19 +1,13 @@
 package com.example.alexis.snowtam.Views;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-/**
- * Created by Mactrasher on 17/11/2016.
- */
-
-public class DisplayActivity extends AppCompatActivity {
+public class DisplaySTActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);
+        setContentView(R.layout.activity_display_st);
     }
-
-
 }
